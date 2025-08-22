ee78f7c8fe09a61227eaaef944c213b08689226d
rma010101 <roslan.amin@gmail.com>
2025-08-22 19:42:24 +0800

    add docker

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/rma010101/ecommerce-product-page'
+  base: '/',
+  //base: 'https://github.com/rma010101/ecommerce-product-page'
 })
